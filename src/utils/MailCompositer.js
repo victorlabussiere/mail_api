559f525062d28e8068d230100f198c22cac0a065
Victor Labussiere <victor.labussiere95@gmail.com>
2023-03-22 21:13:32 -0300

    adm email envelope fixed

--- src/utils/MailCompositer.js
+++ src/utils/MailCompositer.js
@@ -15,17 +15,21 @@ export class MailCompositer {
         this.admMessage = {
             from: this.serverConfig.auth.user,
             to: client.email,
+            replyTo: client.email,
             subject: `Mensagem de ${client.name}`,
             text: client.text,
-            replyTo: client.email,
+            envelope: {
+                from: this.serverConfig.auth.user,
+                to: process.env.ADM_MAIL,
+            }
         }
 
         this.clientMessage = {
             from: this.serverConfig.auth.user,
             to: client.email,
+            replyTo: process.env.ADM_MAIL,
             subject: `Obrigado por usar minha plataforma!`,
             text: `Obrigado, ${client.name} pela mensagem! Caso tenha sido solicitado, entrarei em contato!`,
-            replyTo: process.env.ADM_MAIL,
             envelope: {
                 from: this.serverConfig.auth.user,
                 to: client.email,
